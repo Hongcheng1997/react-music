1f18775aa950affa91ec381c56fd67586766f5e5
高洪城 <18367733277@163.com>
2020-02-10 22:12:48 +0800

    代码优化

--- src/components/progress/progress.js
+++ src/components/progress/progress.js
@@ -4,85 +4,85 @@ import style from './progress.module.scss'
 import { formatTime } from '@/common/helper/utils'
 
 class Progress extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            musicTime: 0,
-            timer: null,
-            padding: false
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      musicTime: 0,
+      timer: null,
+      padding: false
     }
+  }
 
-    componentDidMount() {
-        this.barLeft = ReactDOM.findDOMNode(this.refs._bar).offsetLeft
-        this.barWidth = ReactDOM.findDOMNode(this.refs._bar).offsetWidth
-        this.pointWidthHalf = ReactDOM.findDOMNode(this.refs._point).clientWidth / 2
-    }
+  render() {
+    const { currentTime, proportion, totalTime, showTimer } = this.props
+    return (
+      <div className={style.progress}>
+        <div
+          className={style.bar}
+          ref="_bar"
+          onMouseDown={(e) => this.onMouseDown(e)}
+          onMouseMove={(e) => this.onMouseMove(e)}
+          onMouseUp={(e) => this.onMouseUp(e)}
+        >
+          <div className={style.inner} ref="_inner" style={{ width: proportion * 100 + '%' }}>
+            <span ref="_point" className={style.point}>
+              <span className={style.pointInner}></span>
+            </span>
+          </div>
+        </div>
+        {showTimer && <span className={style.time}>{`${formatTime(currentTime)} / ${formatTime(totalTime)}`}</span>}
+      </div>
+    )
+  }
 
-    refreshTime() {
-        clearInterval(this.state.timer)
+  componentDidMount() {
+    this.barLeft = ReactDOM.findDOMNode(this.refs._bar).offsetLeft
+    this.barWidth = ReactDOM.findDOMNode(this.refs._bar).offsetWidth
+    this.pointWidthHalf = ReactDOM.findDOMNode(this.refs._point).clientWidth / 2
+  }
+
+  refreshTime() {
+    clearInterval(this.state.timer)
+    this.setState({
+      musicTime: 0
+    })
+    this.setState({
+      timer: setInterval(() => {
         this.setState({
-            musicTime: 0
+          musicTime: this.state.musicTime + 1
         })
-        this.setState({
-            timer: setInterval(() => {
-                this.setState({
-                    musicTime: this.state.musicTime + 1
-                })
-            }, 1000)
-        })
-    }
+      }, 1000)
+    })
+  }
 
-    onMouseDown(e) {
-        const percent = this.setPoint(e)
-        this.props.setPoint(percent)
-        this.setState({
-            padding: true
-        })
-    }
+  onMouseDown(e) {
+    const percent = this.setPoint(e)
+    this.props.setPoint(percent)
+    this.setState({
+      padding: true
+    })
+  }
 
-    onMouseMove(e) {
-        if (this.state.padding) {
-            this.setPoint(e)
-        }
+  onMouseMove(e) {
+    if (this.state.padding) {
+      this.setPoint(e)
     }
+  }
 
-    onMouseUp(e) {
-        const percent = this.setPoint(e)
-        this.props.setPoint(percent)
-        this.setState({
-            padding: false
-        })
-    }
+  onMouseUp(e) {
+    const percent = this.setPoint(e)
+    this.props.setPoint(percent)
+    this.setState({
+      padding: false
+    })
+  }
 
-    setPoint(e) {
-        const X = Math.min(Math.max(e.pageX, this.barLeft - this.pointWidthHalf), this.barLeft + this.barWidth - this.pointWidthHalf)
-        const percent = (X - this.barLeft + this.pointWidthHalf) / this.barWidth
-        ReactDOM.findDOMNode(this.refs._inner).style.width = percent * 100 + '%'
-        return percent
-    }
-
-    render() {
-        const { currentTime, proportion, totalTime, showTimer } = this.props
-        return (
-            <div className={style.progress}>
-                <div
-                    className={style.bar}
-                    ref="_bar"
-                    onMouseDown={(e) => this.onMouseDown(e)}
-                    onMouseMove={(e) => this.onMouseMove(e)}
-                    onMouseUp={(e) => this.onMouseUp(e)}
-                >
-                    <div className={style.inner} ref="_inner" style={{ width: proportion * 100 + '%' }}>
-                        <span ref="_point" className={style.point}>
-                            <span className={style.pointInner}></span>
-                        </span>
-                    </div>
-                </div>
-                {showTimer && <span className={style.time}>{`${formatTime(currentTime)} / ${formatTime(totalTime)}`}</span>}
-            </div>
-        )
-    }
+  setPoint(e) {
+    const X = Math.min(Math.max(e.pageX, this.barLeft - this.pointWidthHalf), this.barLeft + this.barWidth - this.pointWidthHalf)
+    const percent = (X - this.barLeft + this.pointWidthHalf) / this.barWidth
+    ReactDOM.findDOMNode(this.refs._inner).style.width = percent * 100 + '%'
+    return percent
+  }
 }
 
 export default Progress
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -6,18 +6,18 @@ class Sidebar extends React.Component {
   constructor() {
     super()
     this.state = {
-      activeId: 0
+      activeId: 0,
+      list: [
+        { path: '/discover', name: '个性推荐' },
+        { path: '/ranking-list', name: '排行榜' },
+        { path: '/singer', name: '歌手' },
+        { path: '/song-sheet', name: '歌单' }
+      ]
     }
   }
 
   render() {
-    const list = [
-      { path: '/discover', name: '个性推荐' },
-      { path: '/ranking-list', name: '排行榜' },
-      { path: '/singer', name: '歌手' },
-      { path: '/song-sheet', name: '歌单' }
-    ]
-    const { activeId } = this.state
+    const { activeId, list } = this.state
     return (
       <div className={style.sideInner}>
         {list.map((item, index) => (
@@ -25,7 +25,7 @@ class Sidebar extends React.Component {
             key={index}
             className={`${style.item} ${
               activeId === index ? style.active : ''
-            }`}
+              }`}
             onClick={() => this.select(index)}
           >
             <Link to={item.path}>
