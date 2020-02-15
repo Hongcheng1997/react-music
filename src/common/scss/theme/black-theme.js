ca33849d331de26b3494628ff81c53942805b57f
高洪城 <18367733277@163.com>
2020-02-15 22:37:29 +0800

    主题切换

--- src/common/scss/theme/black-theme.js
+++ src/common/scss/theme/black-theme.js
@@ -1,60 +1,29 @@
 export default {
-  '--body-bgcolor': '#252525',
+  '--body-bgcolor': '#1D1D1F',
   '--light-bgcolor': '#1ECEA0',
 
-  '--font-color': '#b1b1b1',
-  '--font-color-shallow': '#6f6f6f',
-  '--font-color-white': '#dcdde4',
-  '--font-color-grey': '#5C5C5C',
-  '--font-color-grey2': '#808080',
-  '--font-color-grey-shallow': '#727272',
-  '--border': '#3F3F3F',
+  // scrollbar
   '--scrollbar-color': '#3a3a3a',
-  '--round-hover-bgcolor': '#373737',
-  '--stripe-bg': '#323232',
-  '--shallow-theme-bgcolor': '#2D2625',
-  '--shallow-theme-bgcolor-hover': '#352726',
 
-  //header
+  // header
   '--header-bgcolor': '#252525',
   '--header-font-color': '#fff',
-  '--header-input-color': '#b1b1b1',
-  '--header-input-bgcolor': '#4B4B4B',
-  '--header-input-placeholder-color': '#727272',
 
-  //menu
-  '--menu-bgcolor': '#202020',
-  '--menu-item-hover-bg': '#1d1d1d',
-  '--menu-item-active-bg': '#1b1b1b',
+  // sideBar
+  '--sideBar-font-color': '#D1D1D1',
 
-  //player
-  '--player-bgcolor': '#252525',
+  // song-sheet
+  '--song-sheet-bgcolor': '#2E2E30',
+  '--song-sheet-font-color': '#fff',
 
-  //playlist
-  '--playlist-bgcolor': '#363636',
-  '--playlist-hover-bgcolor': '#2e2e2e',
+  // mini-play
+  '--mini-play-bgcolor': '#1D1D1F',
+  '--mini-play-font-color': '#fff',
 
-  //search
-  '--search-bgcolor': '#363636',
+  // music-tab
+  '--music-tab-bgcolor': '1D1D1F',
+  '--music-tab-font-color': '#fff',
 
-  //progress
-  '--progress-bgcolor': '#232323',
-
-  //input
-  '--input-color': '#b1b1b1',
-  '--input-bgcolor': '#4B4B4B',
-
-  //button
-  '--button-border-color': '#454545',
-  '--button-hover-bgcolor': '#3E3E3E',
-  //tab
-  '--tab-item-color': '#797979',
-  '--tab-item-hover-color': '#B4B4B4',
-  '--tab-item-active-color': '#fff',
-  //modal
-  '--modal-bg-color': '#202020',
-  // prompt
-  '--prompt-bg-color': '#363636',
-  //song-detail
-  '--song-shallow-grey-bg': '#2A2A2A',
+  // progress
+  '--progress-bar-bgcolor': '#505051'
 }
--- src/common/scss/theme/white-theme.js
+++ src/common/scss/theme/white-theme.js
@@ -1,8 +1,28 @@
 export default {
   '--body-bgcolor': '#fff',
   '--light-bgcolor': '#1ECEA0',
+  // scrollbar
+  '--scrollbar-color': '#D0D0D0',
 
-    //header
-    '--header-bgcolor': '#fff',
-    '--header-font-color': '$gray-medium',
+  // header
+  '--header-bgcolor': '#fff',
+  '--header-font-color': '$gray-medium',
+
+  // sideBar
+  '--sideBar-font-color': '$gray-medium',
+
+  // song-sheet
+  '--song-sheet-bgcolor': '#E1E1E2',
+  '--song-sheet-font-color': '#000000',
+
+  // mini-play
+  '--mini-play-bgcolor': '$gray-background',
+  '--mini-play-font-color': '$gray-deep',
+
+  // music-tab
+  '--music-tab-bgcolor': '$gray-background',
+  '--music-tab-font-color': '$gray-deep',
+
+  // progress
+  '--progress-bar-bgcolor': '$gray-light'
 }
